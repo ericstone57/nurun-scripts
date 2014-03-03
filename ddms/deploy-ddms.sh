fb9b3a794b759ea14b1720b6febd598b650c14d6
Eric Lee <eric.lee@nurun.com>
2014-03-03 21:41:48 +0800

    fixed DDMS deployment script

--- ddms/deploy-ddms.sh
+++ ddms/deploy-ddms.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-set -x
+
+# Please run under script's directory but parent
+# By Eric.Lee
+
+#set -x
+
 PROJECT=~/sites/ddms
 BRANCH=production
+LOCAL_PATH=`pwd`
 
 cd $PROJECT
 git fetch
 git reset --hard origin/$BRANCH
 git pull origin $BRANCH
-cp ~/nurun-scripts/ddms/prod_config/settings-ddms.php sites/default/settings.php
-rm -rf sites/default/modules/ddms_performance/speedy/js
-drush smj uglifyjs
+# system setting
+cp $LOCAL_PATH/prod_config/settings-ddms.php $PROJECT/sites/default/settings.php
+# sass convert and compressor
+cd $PROJECT/sites/default/themes/ddms && compass clean && compass compile -c config.prod.rb --force
+cd $PROJECT
+# js parser and compressor
+rm -rf $PROJECT/sites/default/modules/ddms_performance/ddms_uglifyjs/js && drush smj uglifyjs
+# clean cache
+cd $PROJECT
 drush cc all
