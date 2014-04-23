4535f0ea55db686e0ed743b242035286fef5e269
Eric Lee <eric.lee@nurun.com>
2014-04-24 01:03:50 +0800

    add Lancome Effortless Chic deployment script

---
+++ efc/deploy-prod.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# By Eric.Lee
+
+set -x
+
+PROD_SRV=192.168.10.37
+
+cd ~/sites/sha-lancome-efforless-chic
+
+rsync -arvP --delete \
+  --exclude=web/upload \
+  --exclude=*.json \
+  --exclude=*.lock \
+  --exclude=.git \
+  --exclude=*.rb \
+  --exclude=manifests \
+  --exclude=nurun-site-build \
+  --exclude=sass \
+  --exclude=*.md \
+  --exclude=Vagrantfile \
+  . $PROD_SRV:~/sites/efc/
